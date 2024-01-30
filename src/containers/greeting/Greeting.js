002c06975c867f2ba6ffb6ba11a2916b6dbb3c55
Andres G <agh92@hotmail.com>
2024-01-30 17:15:44 +0100

    disable github and disable contact me

--- src/containers/greeting/Greeting.js
+++ src/containers/greeting/Greeting.js
@@ -38,7 +38,7 @@ export default function Greeting() {
                 {greeting.subTitle}
               </p>
               <SocialMedia />
-              <div className="button-greeting-div">
+              {/* <div className="button-greeting-div">
                 <Button text="Contact me" href="#contact" />
                 {greeting.resumeLink && (
                   <Button
@@ -47,7 +47,7 @@ export default function Greeting() {
                     href={greeting.resumeLink}
                   />
                 )}
-              </div>
+              </div> */}
             </div>
           </div>
           <div className="greeting-image-div">
--- src/containers/profile/Profile.js
+++ src/containers/profile/Profile.js
@@ -1,6 +1,5 @@
 import React, {useState, useEffect, lazy, Suspense} from "react";
 import {openSource} from "../../portfolio";
-import Contact from "../contact/Contact";
 import Loading from "../loading/Loading";
 
 const renderLoader = () => <Loading />;
@@ -47,6 +46,6 @@ export default function Profile() {
       </Suspense>
     );
   } else {
-    return <Contact />;
+    return <></>;
   }
 }
